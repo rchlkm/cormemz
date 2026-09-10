b0a0129a2158336192167543f93080b13e9fe993
Rachel Kim <rachelkim.tech@gmail.com>
2026-09-09 21:51:48 -0700

    Initial Commit

---
+++ CoreMems/CoreMemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMemsApp.swift
+//  CoreMems
+//
+//  Created by Rachel Kim on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreMemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
